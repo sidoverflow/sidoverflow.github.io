66e469f7cbdbd174af46c18fd863fee615b1d36d
Sidharth Sudarsan <git@sidharth.me>
2022-02-13 21:47:13 -0500

    add malayalam toggle

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,28 @@ function copyCommand() {
     const curlCommand = "curl https://sidharth.me/sidharth-pubkey.asc | gpg--import";
     navigator.clipboard.writeText(curlCommand);
 }
+
+window.setInterval(() => {
+    this.toggle()
+}, 3500)
+
+const greeting = "Hi, I'm";
+const name = 'Sidharth Sudarsan.';
+const isMalayalam = false;
+
+function toggle() {
+    if (this.name === 'Sidharth Sudarsan.') {
+        this.greeting = 'നമസ്കാരം, ഞാൻ'
+        this.name = 'സിദ്ധാർഥ് സുദർശൻ.'
+        this.isMalayalam = true
+    } else {
+        this.greeting = "Hi, I'm"
+        this.name = 'Sidharth Sudarsan.'
+        this.isMalayalam = false
+    }
+    var greetingDiv = document.getElementById('greeting');
+    greetingDiv.innerText = this.greeting;
+
+    var nameDiv = document.getElementById('name');
+    nameDiv.innerText = this.name;
+}
